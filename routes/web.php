1d904bc7326ab1b83ab7a5373c5f3d1ef70d9c56
droldan <droldan@socialnet.com.ar>
2026-02-28 22:14:32 -0300

    feat: definir rutas y controladores thin para Home, Movies, Series y Channels

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,16 @@
 <?php
 
+use App\Http\Controllers\ChannelsController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\MovieDetailController;
+use App\Http\Controllers\MoviesController;
+use App\Http\Controllers\SeriesController;
+use App\Http\Controllers\SeriesDetailController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
-
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', HomeController::class)->name('home');
+Route::get('/peliculas', MoviesController::class)->name('movies.index');
+Route::get('/pelicula/{id}', MovieDetailController::class)->name('movies.show');
+Route::get('/series', SeriesController::class)->name('series.index');
+Route::get('/serie/{id}', SeriesDetailController::class)->name('series.show');
+Route::get('/canales', ChannelsController::class)->name('channels.index');
